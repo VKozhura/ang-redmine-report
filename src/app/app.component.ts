21b342a2ea2aa331b7ba3d735ff2989f44952887
VKozhura <kozhura_v@mail.ru>
2022-08-23 02:11:22 +0300

    получение данных из redmine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,45 @@
 import { Component } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+export interface Task {
+  id: number;
+  last_name: string;
+  email: string;
+  avatar: any;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  tasks: any = [];
+  startDate: string = '';
+  start2Date: string = '';
+  token: string = '';
+  private http: HttpClient;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  getTasks() {
+    console.log('клик');
+
+    const headers = new HttpHeaders().set('X-Redmine-API-Key', this.token);
+    const options = { headers: headers };
+
+    this.http
+      .get(
+        `https://redmine.bivgroup.com/issues.json?status_id=*&offset=1&limit=100&created_on=%3E%3C${this.startDate}|${this.start2Date}`,
+        options
+      )
+      .subscribe((response: any) => {
+        console.log(response);
+
+        this.tasks = response.issues;
+      });
+  }
+}
