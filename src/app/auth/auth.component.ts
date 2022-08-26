2e0169fad94cbaba4efc117d18380ebdbe41d7fe
VKozhura <kozhura_v@mail.ru>
2022-08-26 17:59:45 +0300

    fix

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { RedmineService } from '../services/redmine.service';
+
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
   token: string = '';
+  startDate: string = '';
+  start2Date: string = '';
+  tasks: any = []
+  
+  constructor(private redmineService: RedmineService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  getTasks() {
+    this.redmineService.getTasks(this.token, this.startDate, this.start2Date).subscribe()
+  }
+  
 }
--- src/app/services/redmine.service.ts
+++ src/app/services/redmine.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RedmineService {
+  tasks$: Observable<any>
   private http: HttpClient;
-
+   
   constructor(http: HttpClient) {
     this.http = http;
   }
@@ -14,9 +16,11 @@ export class RedmineService {
   getTasks(token: string, date1: string, date2: string) {
     const headers = new HttpHeaders().set('X-Redmine-API-Key', token);
     const options = { headers: headers };
-    return this.http.get(
+    this.tasks$ = this.http.get(
       `https://redmine.bivgroup.com/issues.json?status_id=*&offset=0&limit=100&created_on=%3E%3C${date1}|${date2}`,
       options
     );
+
+    return this.tasks$;
   }
 }
