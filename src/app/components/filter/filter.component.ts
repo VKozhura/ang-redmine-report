eeec4b780b1777962d9f671dd56645a937c444fb
VKozhura <kozhura_v@mail.ru>
2022-09-20 23:29:09 +0300

    new fix

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
 import { TasksService } from 'src/app/services/tasks.service';
 
 interface Tracker {
@@ -11,8 +12,10 @@ interface Tracker {
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
 })
-export class FilterComponent {
-  tracker: string = 'Все';
+export class FilterComponent implements OnInit {
+  tracker: BehaviorSubject<string>;
+  filteredTasks: Observable<any[]>;
+  // tracker: string = 'Все';
   trackers: Tracker[] = [
     { id: 1, name: 'Все' },
     { id: 1, name: 'Поддержка' },
@@ -21,10 +24,29 @@ export class FilterComponent {
   ];
   constructor(private tasksService: TasksService) {}
 
-  onSelectTracker(tracker: string) {
-    console.log('селект');
+  ngOnInit() {
+    this.tracker = new BehaviorSubject('Все');
+    this.filteredTasks = this.createFilterTasks(
+      this.tracker,
+      this.tasksService.tasks
+    );
+  }
 
-    this.tasksService.filterTrackers(tracker);
+  createFilterTasks(tracker$: Observable<string>, tasks: Observable<any[]>) {
+    return combineLatest(tasks, tracker$, (tasks, tracker: string) => {
+      // this is the project function where we imperatively
+      // implement the filtering logic
+      if (tracker === 'Все') return tasks;
+      return tasks.filter(
+        (task) => task.tracker.name.toLowerCase() === tracker.toLowerCase()
+      );
+    });
+  }
+
+  onSelectTracker(value: string) {
+    console.log('селект');
+    this.tracker.next(value);
+    // this.tasksService.filterTrackers(tracker);
     console.log(this.tasksService.tasks);
   }
 }
